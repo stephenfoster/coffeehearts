36256770f4b54bf0b65076c7ef04ae98b4b54e43
Your Name <stephen.foster4@gmail.com>
2009-06-01 11:49:27 -0500

    Fixed account controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,7 +3,7 @@ class AccountController < ApplicationController
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
-  skip_before_filter :login_required, :only => [:login]
+  skip_before_filter :login_required, :only => [:login, :new, :signup]
 
   # say something nice, you goof!  something sweet.
   def index
